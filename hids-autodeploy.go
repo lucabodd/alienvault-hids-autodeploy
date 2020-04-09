26bb9bd2b8e4f192a8e65b6b3d343731f0e9b5ee
Luca Bodini <lucabodd@gmail.com>
2020-04-09 23:33:20 +0200

    tmp

--- hids-autodeploy.go
+++ hids-autodeploy.go
@@ -353,6 +353,7 @@ func sshCopyId(ip string, port string, ssh_username string, ssh_password string,
 	result, warnings, err := scanner.Run()
 	check(err)
 
+	fmt.Println(result.Hosts)
 	if result.Hosts != nil {
 		if warnings != nil {
 			fmt.Printf("[!] \n %v", warnings)
